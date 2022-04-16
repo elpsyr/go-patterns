a0604d0c23655a82bb6a4a67f929c1b300ec6626
he11c <13735599246@163.com>
2022-04-16 18:40:23 +0800

    add interpreter

---
+++ 19_interpreter/interpreter.go
@@ -0,0 +1,92 @@
+package interpreter
+
+import (
+	"strconv"
+	"strings"
+)
+
+type Node interface {
+	Interpret() int
+}
+
+type ValNode struct {
+	val int
+}
+
+func (n *ValNode) Interpret() int {
+	return n.val
+}
+
+type AddNode struct {
+	left, right Node
+}
+
+func (n *AddNode) Interpret() int {
+	return n.left.Interpret() + n.right.Interpret()
+}
+
+type MinNode struct {
+	left, right Node
+}
+
+func (m *MinNode) Interpret() int {
+	return m.left.Interpret() - m.right.Interpret()
+}
+
+type Parser struct {
+	exp   []string
+	index int
+	prev  Node
+}
+
+func (p *Parser) Parse(exp string) {
+	p.exp = strings.Split(exp, " ")
+	// [1 + 2 + 3 - 4 + 5 - 6]
+	for {
+		if p.index >= len(p.exp) {
+			return
+		}
+		// 1 3 5 7 9 11
+		switch p.exp[p.index] {
+		case "+":
+			p.prev = p.newAddNode()
+		case "-":
+			p.prev = p.newMinNode()
+		default:
+			// start
+			p.prev = p.newValNode()
+		}
+	}
+
+}
+
+// [1] [+] [2] [+] [3] [-] [4] [+] [5] [-] [6]
+//
+
+func (p *Parser) newAddNode() Node {
+	p.index++
+	return &AddNode{
+		left:  p.prev,
+		right: p.newValNode(),
+	}
+}
+
+func (p *Parser) newMinNode() Node {
+	p.index++
+	return &MinNode{
+		left:  p.prev,
+		right: p.newValNode(),
+	}
+}
+
+func (p *Parser) newValNode() Node {
+	v, _ := strconv.Atoi(p.exp[p.index])
+	p.index++
+	return &ValNode{
+		val: v,
+	}
+}
+
+func (p *Parser) Result() Node {
+	return p.prev
+}
