cac0afbec7a701d6ff9995b5c8f307189e81110c
he11c <13735599246@163.com>
2022-04-25 10:41:40 +0800

    add chain

---
+++ 21_chain_of_responsibility/chain.go
@@ -0,0 +1,99 @@
+package chain
+
+import "fmt"
+
+type Manager interface {
+	HaveRight(money int) bool
+	HandleFeeRequest(name string, money int) bool
+}
+
+type RequestChain struct {
+	Manager
+	successor *RequestChain
+}
+
+func (r *RequestChain) SetSuccessor(m *RequestChain) {
+	r.successor = m
+
+}
+
+func (r *RequestChain) HaveRight(money int) bool {
+	return true
+}
+
+func (r *RequestChain) HandleFeeRequest(name string, money int) bool {
+	if r.Manager.HaveRight(money) {
+		return r.Manager.HandleFeeRequest(name, money)
+	}
+	if r.successor != nil {
+		return r.successor.HandleFeeRequest(name, money)
+
+	}
+	return false
+}
+
+type ProjectManager struct {
+}
+
+func NewProjectManagerChain() *RequestChain {
+	return &RequestChain{
+		Manager: &ProjectManager{},
+	}
+}
+
+func (p *ProjectManager) HaveRight(money int) bool {
+	return money < 500
+}
+
+func (p *ProjectManager) HandleFeeRequest(name string, money int) bool {
+	if name == "bob" {
+		fmt.Printf("Project manager permit %s %d fee request\n", name, money)
+		return true
+	}
+	fmt.Printf("Project manager don't permit %s %d fee request\n", name, money)
+	return false
+}
+
+type DepManager struct {
+}
+
+func NewDepManagerChain() *RequestChain {
+	return &RequestChain{
+		Manager: &DepManager{},
+	}
+}
+
+func (p *DepManager) HaveRight(money int) bool {
+	return money < 5000
+}
+
+func (p *DepManager) HandleFeeRequest(name string, money int) bool {
+	if name == "tom" {
+		fmt.Printf("Dep manager permit %s %d fee request\n", name, money)
+		return true
+	}
+	fmt.Printf("Dep manager don't permit %s %d fee request\n", name, money)
+	return false
+}
+
+type GeneralManager struct {
+}
+
+func NewGeneralManagerChain() *RequestChain {
+	return &RequestChain{
+		Manager: &GeneralManager{},
+	}
+}
+
+func (p *GeneralManager) HaveRight(money int) bool {
+	return true
+}
+
+func (p *GeneralManager) HandleFeeRequest(name string, money int) bool {
+	if name == "ada" {
+		fmt.Printf("General manager permit %s %d fee request\n", name, money)
+		return true
+	}
+	fmt.Printf("General manager don't permit %s %d fee request\n", name, money)
+	return false
+}
